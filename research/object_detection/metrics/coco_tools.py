fed9a7e7b8c1dbd99f0cccd1c87f9d5141f89e4a
Hisho Rajanathan <hishorajanathan15@gmail.com>
2020-09-10 16:38:45 +0100

    include_metrics_per_category is True
    Followed https://github.com/pialin/MetricsPerCategoryImplementation/blob/master/tensorflow/research/object_detection/metrics/coco_tools.py line 240 - 244

--- research/object_detection/metrics/coco_tools.py
+++ research/object_detection/metrics/coco_tools.py
@@ -256,6 +256,11 @@ class COCOEvalWrapper(cocoeval.COCOeval):
     self.evaluate()
     self.accumulate()
     self.summarize()
+    
+     # add for metrics per catergory from here
+    if include_metrics_per_category is True:
+        self.summarize_per_category()
+    # add for metrics per category end here
 
     summary_metrics = {}
     if self._iou_type in ['bbox', 'segm']:
